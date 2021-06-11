bc8d2cfd56fe6a96324940f3413390d50599e6af
Kereki László <kereki.laszlo@molaris.hu>
2021-06-11 12:33:38 +0200

    :art: add CrossOrigin annotation

--- src/main/java/hu/xaddew/lovelyletter/controller/MainController.java
+++ src/main/java/hu/xaddew/lovelyletter/controller/MainController.java
@@ -16,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = { "http://localhost:4200" })
 public class MainController {
 
   @Value("${lovely-letter-rules-url}")
