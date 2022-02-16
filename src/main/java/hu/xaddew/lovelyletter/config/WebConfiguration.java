9757a56cb03b5128aea86502dcac671fc935c0ba
Kereki László <kereki.laszlo@molaris.hu>
2022-02-16 22:06:04 +0100

    :art: refactor webMvcConfiguration

--- src/main/java/hu/xaddew/lovelyletter/config/WebConfiguration.java
+++ src/main/java/hu/xaddew/lovelyletter/config/WebConfiguration.java
@@ -1,15 +1,17 @@
 package hu.xaddew.lovelyletter.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 @Configuration
-public class WebConfiguration extends WebMvcConfigurationSupport {
+public class WebConfiguration implements WebMvcConfigurer {
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("/**")
-        .addResourceLocations("classpath:/static/");
+    registry.addResourceHandler("/images/**")
+            .addResourceLocations("classpath:/static/images/");
   }
 }
